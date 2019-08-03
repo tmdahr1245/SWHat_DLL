b166503b4712394c573933680f4595d9346b6620
김승목 <tmdahr1245@naver.com>
2019-08-03 23:46:41 +0900

    190803

--- SWHat_DLL/Log.cpp
+++ SWHat_DLL/Log.cpp
@@ -21,57 +21,77 @@ HANDLE hMutex, hLogFile;
 wchar_t log_name[11];
 BOOL startLogging = FALSE;
 
+StringBuffer sMain;
+PrettyWriter<StringBuffer> MainWriter(sMain);
 void LogFileOpen() {
-	StringBuffer s;
-	Writer<StringBuffer> writer(s);
 	hMutex = CreateMutex(NULL, FALSE, NULL);
 	DWORD pid = GetCurrentProcessId();
 	wchar_t buf[10];
+	DWORD dwByte;
 	lstrcpy(log_name, L"C:\\");
 	lstrcat(log_name, _ultow(pid, buf, 10));
 	lstrcat(log_name, L".json");
 	hLogFile = CreateFile(log_name, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	writer.StartObject();
-	writer.Key("pid");
-	writer.Uint((unsigned int)pid);
-	writer.Key("process");
-	writer.String(GetProcessName(pid));
-	writer.EndObject();
-	DWORD dwByte;
-	WriteFile(hLogFile, s.GetString(), strlen(s.GetString()), &dwByte, NULL);
-	WriteFile(hLogFile, "\n", strlen("\n"), &dwByte, NULL);
-	startLogging = TRUE;
+	
+	MainWriter.StartObject();
+	WriteFile(hLogFile, sMain.GetString(), sMain.GetLength(), &dwByte, NULL);
+	sMain.Clear();
 
+	MainWriter.Key("pid");
+	MainWriter.Uint((unsigned int)pid);
+	WriteFile(hLogFile, sMain.GetString(), sMain.GetLength(), &dwByte, NULL);
+	sMain.Clear();
+
+	MainWriter.Key("process");
+	MainWriter.String(GetProcessName(pid));
+	WriteFile(hLogFile, sMain.GetString(), sMain.GetLength(), &dwByte, NULL);
+	sMain.Clear();
+
+	MainWriter.Key("log");
+	MainWriter.StartArray();
+	WriteFile(hLogFile, sMain.GetString(), sMain.GetLength(), &dwByte, NULL);
+	sMain.Clear();
+
+	startLogging = TRUE; 
 }
 void LogFileClose() {
+	DWORD dwByte;
+	MainWriter.EndArray();
+	MainWriter.EndObject();
+	WriteFile(hLogFile, sMain.GetString(), sMain.GetLength(), &dwByte, NULL);
 	CloseHandle(hLogFile);
 }
 void Log(vector<pair<string,string>> v, wchar_t* buf, ULONG Length, const char* name) {
 	if (startLogging) {
 		WaitForSingleObject(hMutex, INFINITE);
+
 		SYSTEMTIME cur_time;
+		DWORD dwByte;
+
 		GetLocalTime(&cur_time);
 		char time[100];
 		sprintf(time, "[%02d:%02d:%02d.%03d]", cur_time.wHour, cur_time.wMinute, cur_time.wSecond, cur_time.wMilliseconds);
-		StringBuffer s;
-		Writer<StringBuffer> writer(s);
-		writer.StartObject();
-		writer.Key("time");
-		writer.String(time);
+
+		StringBuffer sLog;
+		Writer<StringBuffer> LogWriter(sLog);
+
+		LogWriter.StartObject();
+		LogWriter.Key("time");
+		LogWriter.String(time);
+
 		for (auto& i : v) {
-			writer.Key(i.first.c_str());
-			writer.String(i.second.c_str());
+			LogWriter.Key(i.first.c_str());
+			LogWriter.String(i.second.c_str());
 		}
 		if (name) {
-			writer.Key(name);
-			writer.RawNumber((char*)buf, Length);
+			LogWriter.Key(name);
+			LogWriter.RawNumber((char*)buf, Length);
 		}
 
-		writer.EndObject();
-
-		DWORD dwByte;
-		WriteFile(hLogFile, s.GetString(), strlen(s.GetString()), &dwByte, NULL);
-		WriteFile(hLogFile, "\n", strlen("\n"), &dwByte, NULL);
+		LogWriter.EndObject();
+		MainWriter.RawValue(sLog.GetString(), sLog.GetLength(), kObjectType);
+		WriteFile(hLogFile, sMain.GetString(), sMain.GetLength(), &dwByte, NULL);
+		sMain.Clear();
 		ReleaseMutex(hMutex);
 	}
 }
